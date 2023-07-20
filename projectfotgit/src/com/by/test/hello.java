b87088f8884ee0c832ed9892defbdbc4d6e123aa
Redoer <3046872004@qq.com>
2023-07-20 21:48:30 +0800

    第一次提交

---
+++ projectfotgit/src/com/by/test/hello.java
@@ -0,0 +1,9 @@
+package com.by.test;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("12323123");
+        System.out.println("12323123");
+        System.out.println("12323123");
+    }
+}
